0dcc6b23c778bb1ae81b50a67bef013c050866eb
Haziq Kamel <haziqkamel@live.com>
2023-07-14 10:36:28 +0800

    slots app

---
+++ section-6/slots/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <app-form />
+</template>
+
+<script>
+import AppForm from "./components/Form.vue"
+
+export default {
+  name: "App",
+  components: {
+    AppForm,
+  }
+}
+</script>
+
+
